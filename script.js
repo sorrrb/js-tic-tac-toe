767593902cd7907b495391aff54ffc8caa14314d
sorrrb <cdrukepike@gmail.com>
2022-09-13 14:33:30 -0400

    Implement basic menu & game end functionality

--- script.js
+++ script.js
@@ -1,32 +1,113 @@
 const gameBoard = (() => {
-  const MARKERS = [`X`, `O`];
   let playingBoard = [``, ``, ``, ``, ``, ``, ``, ``, ``]; // zero-based 0-8
   const sendMoveToBoard = (index, marker) => {
     playingBoard[index] = marker;
   };
 
-  return { MARKERS, sendMoveToBoard };
+  const clearPlayingBoard = () => {
+    playingBoard = [``, ``, ``, ``, ``, ``, ``, ``, ``];
+  }
+
+  const checkEnd = () => {
+
+    let boardReference = playingBoard.slice();
+
+    if ((boardReference[0]) && (boardReference[0] === boardReference[1]) && (boardReference[1] === boardReference[2])) { // Horizontal win conditions
+      console.log('Win');
+    }
+    else if ((boardReference[3]) && (boardReference[3] === boardReference[4]) && (boardReference[4] === boardReference[5])) {
+      console.log('Win');
+    }
+    else if ((boardReference[6]) && (boardReference[6] === boardReference[7]) && (boardReference[7] === boardReference[8])) {
+      console.log('Win');
+    }
+
+    else if ((boardReference[0]) && (boardReference[0] === boardReference[3]) && (boardReference[3] === boardReference[6])) { // Vertical win conditions
+      console.log('Win');
+    }
+    else if ((boardReference[1]) && (boardReference[1] === boardReference[4]) && (boardReference[4] === boardReference[7])) {
+      console.log('Win');
+    }
+    else if ((boardReference[2]) && (boardReference[2] === boardReference[5]) && (boardReference[5] === boardReference[8])) {
+      console.log('Win');
+    }
+
+    else if ((boardReference[0]) && (boardReference[0] === boardReference[4]) && (boardReference[4] === boardReference[8])) { // Diagonal win conditions
+      console.log('Win');
+    }
+    else if ((boardReference[2]) && (boardReference[2] === boardReference[4]) && (boardReference[4] === boardReference[6])) {
+      console.log('Win');
+    }
+
+    else {
+      for (const boardSpace in boardReference) {
+        if (!boardReference[boardSpace]) return; // Ensures board is full
+      }
+      console.log('Draw');
+    }
+
+  }
+
+  return { sendMoveToBoard, checkEnd, clearPlayingBoard };
 })();
 
 const displayController = (() => {
+  let upcomingMarker = `X`;
   const boardCells = document.querySelectorAll(`div.gamecell`);
   boardCells.forEach((cell) => {
+
     cell.addEventListener('click', function() {
+
       const indexRef = cell.dataset.index;
-      console.log(indexRef);
+      if (cell.textContent) return; // Return if move is played in space
+      gameBoard.sendMoveToBoard(indexRef, upcomingMarker); // Update gameboard
+      cell.textContent = upcomingMarker; // Update display
+
+      switch(upcomingMarker) { // Update new display marker
+        case `X`:
+          upcomingMarker = `O`;
+          break;
+        case `O`:
+          upcomingMarker = `X`;
+          break;
+        default:
+          console.log('ERROR');
+          break;
+      }
+      gameBoard.checkEnd();
     });
   })
 
-  const drawMove = (player, index) => {
-    
-  };
+  const resetBoard = () => {
+    gameBoard.clearPlayingBoard();
+    boardCells.forEach((cell) => {
+      cell.textContent = ``;
+    })
+    upcomingMarker = `X`;
+  }
 
-  return { boardCells, drawMove };
+  const openMenuButton = document.querySelector(`button.menu-open`);
+  openMenuButton.addEventListener('click', function() {
+    const modalMenu = document.querySelector(`div.menu`);
+    modalMenu.style.visibility = `visible`;
+  })
+
+  const closeMenuButton = (document.querySelector(`div.menu-close`)).firstElementChild;
+  closeMenuButton.addEventListener('click', function() {
+    const modalMenu = document.querySelector(`div.menu`);
+    modalMenu.style.visibility = `hidden`;
+  })
+
+  const resetButton = document.querySelector(`button.reset`);
+  resetButton.addEventListener('click', resetBoard);
+
+  return { upcomingMarker, boardCells, resetBoard };
 })();
 
 const Player = (name, marker) => {
   const getName = () => name;
   const getMarker = () => marker;
+
   return { getName, getMarker };
 };
 
