1af9da918ccdc1ef33c1f4fa695ecfc2b15c6009
sorrrb <cdrukepike@gmail.com>
2024-01-02 16:02:11 -0500

    Implement base logic for determining winner

--- script.js
+++ script.js
@@ -86,24 +86,76 @@ const gameController = (function () {
     }
   ];
 
+  let winner = null; // Set default winner to null
+  let isGameOver = false; // Set default game state as active
+
+  const setWinner = () => {
+    isGameOver = true;
+    winner = getActivePlayer().name;
+  }
+
   let activePlayer = players[0]; // Set default active player to P1
 
   const getActivePlayer = () => activePlayer;
 
   const switchActivePlayer = () => {
     activePlayer = (activePlayer === players[0] ? players[1] : players[0]);
-  }
+  };
 
   const printNewRound = () => {
     board.printBoard();
     console.log(`It is ${getActivePlayer().name}'s turn.`);
-  }
+  };
 
   const playRound = (row, column) => {
     console.log(`Playing move at [${row}, ${column}]`);
     board.takeSquare(row, column, getActivePlayer().token);
 
-    // Win logic goes here
+    const checkEnd = (function () {
+      const boardState = board.getBoard();
+
+      // Check given object array to see if all objects (squares) value is equal
+      const checkAllEqual = testArr => {
+        const emptySquares = testArr.filter((square) => square.getToken() === 0);
+        if (emptySquares.length > 0) return false; // Check if a square in a given 3x1 line is empty
+
+        return (testArr.every((square) => square.getToken() === testArr[0].getToken() ? true : false)); // https://dev.to/rajnishkatharotiya/function-to-check-if-all-records-are-equal-in-array-javascript-3mo3#:~:text=Javascript%20Useful%20Snippets%20%E2%80%94%20allEqual(),allEqual%20%3D%20arr%20%3D%3E%20arr.
+      };
+
+      // Check board for horizontal wins
+      const checkRow = (function () {
+        const foundWin = boardState.filter((row) => checkAllEqual(row));
+        if (foundWin.length > 0) setWinner();
+      })();
+
+      // Check board for vertical wins
+      const checkCol = (function () {
+        const matrixLength = boardState.length;
+        const reformatBoard = [];
+
+        for (let j = 0; j < matrixLength; j++) { // Transposes dimensions of 2D array
+          reformatBoard[j] = Array(matrixLength);
+          for (let i = 0; i < matrixLength; i++) {
+            reformatBoard[j][i] = boardState[i][j];
+          }
+        }
+
+        const foundWin = reformatBoard.filter((column) => checkAllEqual(column));
+        if (foundWin.length > 0) setWinner();
+      })();
+
+      // Check board for a draw
+      const checkDraw = (function () {
+
+      })();
+    })();
+
+    if (isGameOver) {
+      const hasWinner = (winner === null ? false : true);
+      board.printBoard();
+      if (hasWinner) return (`${winner} wins! Play again?`);
+      return `It's a tie! Play again?`;
+    }
 
     switchActivePlayer(); // Switches player
     printNewRound(); // Starts next round
